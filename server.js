e41a162fa73a3a5aff09491ebb9bb97ee599041b
Glitch (oxidized-cod) <none>
2019-12-23 19:48:39 +0000

    🌧🍴 Checkpoint ./server.js:9636892/203

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", function(req, res){
-  res.json()
+  res.json({"ipaddress": req.header.x})
 });
 
 // listen for requests :)
